ebda58bf08f92be16e6e85de32dbf4c065d3d70a
Maksym <87162463+maksy23@users.noreply.github.com>
2025-11-08 02:02:44 -0500

    feat: add theme toggle

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/utils'
 
+import { ThemeModeToggle } from '../theme-mode-toggle'
 import NavigationMenu from './navigation-menu'
 
 type HeaderProps = {
@@ -9,9 +10,10 @@ type HeaderProps = {
 export default function Header({ className }: HeaderProps) {
   return (
     <header className={cn('w-full p-7', className)}>
-      <div className='mx-auto flex w-xs flex-row'>
+      <div className='mx-auto flex w-xs flex-row items-center'>
         <NavigationMenu />
-        <div>Play@</div>
+        <div className='light:border-black h-4 border-l dark:border-white' />
+        <ThemeModeToggle />
       </div>
     </header>
   )
